058b6c6c0765a00a0d3f7585b12ec19a43063815
havu73 <havu73@ucla.edu>
2022-02-17 16:38:28 -0800

    add some trial data

--- scripts/calculate_diff_rep_state_map.py
+++ scripts/calculate_diff_rep_state_map.py
@@ -10,6 +10,7 @@ NUM_CORES = 4
 
 def read_rep_df(fn, num_chromHMM_state):
 	right_colnames = list(map(lambda x: 'state_' + str(x+1), range(num_chromHMM_state)))
+	print(fn)
 	try:
 		df = pd.read_csv(fn, header = 0, index_col = 0, sep = '\t')
 		assert list(df.columns) == right_colnames, 'index_col = 0 is wrong'
@@ -38,13 +39,32 @@ def get_diff_rep_state_one_process(region_fn_list, group1_folder, group2_folder,
 		get_diff_rep_state_one_genomic_region(region_fn, group1_folder, group2_folder, output_folder, num_chromHMM_state)
 	return 
 
-def get_diff_rep_state_whole_genome(group1_folder, group2_folder, output_folder, num_chromHMM_state):
+def get_genomic_positions_list(group1_folder, output_folder, redo_existing_file):
+	gen_pos_segment_fn_list = glob.glob(group1_folder + '/chr*')
+	gen_pos_list = [(x.split('/')[-1]).split('_avg_pred.txt.gz')[0] for x in gen_pos_segment_fn_list] # get the list of all genomic positions available: [chr9_11, chr9_12, etc.]
+	if redo_existing_file == 1:
+		return gen_pos_list
+	else: # repace_existing_file is 0
+		existing_fn_list = glob.glob(output_folder + '/*_avg_pred.txt.gz')
+		existing_gen_pos_list = list(map(lambda x: (x.split('/')[-1]).split('_avg_pred.txt.gz')[0], existing_fn_list))
+		gen_pos_list = np.setdiff1d(gen_pos_list, existing_gen_pos_list)
+		print("Number of positions that will be calculated in average_pred_results: " + str(len(gen_pos_list)))
+		print(gen_pos_list)
+		return gen_pos_list
+
+def get_diff_rep_state_whole_genome(group1_folder, group2_folder, output_folder, num_chromHMM_state, redo_existing_file):
 	# this function will:
 	# 1. get list of regions(files) inside folder diff_folder, partition into NUM_CORES processes
 	g1_fn_list = os.listdir(group1_folder)
 	g2_fn_list = os.listdir(group2_folder)
+	print(len(g1_fn_list))
+	print(len(g2_fn_list))
 	assert len(g1_fn_list) == len(g2_fn_list), 'Number of files from group 1 and group 2 are not equal'
-	partition_fn_list = helper.partition_file_list(g1_fn_list, NUM_CORES) # partition the list of fn in group1 (list of genomeic regions) into subgroups, so that we can add them into processes later
+	gen_pos_list = get_genomic_positions_list(group1_folder, output_folder, redo_existing_file) # the list of regions that we still need to calculate the differential scores for
+	input_fn_list = list(map(lambda x: x + '_avg_pred.txt.gz', gen_pos_list))
+	print('Number of files to calculate differential scores for: {}'.format(len(input_fn_list)))
+	print(gen_pos_list)
+	partition_fn_list = helper.partition_file_list(input_fn_list, NUM_CORES) # partition the list of fn in group1 (list of genomeic regions) into subgroups, so that we can add them into processes later
 	# 2. For each group of region_fn we will call a process to calculate the difference between two groups' chromatin state assignment
 	processes = [mp.Process(target = get_diff_rep_state_one_process, args = (partition_fn_list[i], group1_folder, group2_folder, output_folder, num_chromHMM_state)) for i in range(NUM_CORES)]
 	for p in processes:
@@ -55,7 +75,7 @@ def get_diff_rep_state_whole_genome(group1_folder, group2_folder, output_folder,
 	return
 
 def main():
-	if len(sys.argv) != 5:
+	if len(sys.argv) != 6:
 		usage()
 	group1_folder = sys.argv[1] # where the representative state maps for group1 are stored. The files in this fodler correspond to different regions in the genome
 	helper.check_dir_exist(group1_folder)
@@ -64,9 +84,11 @@ def main():
 	output_folder = sys.argv[3]
 	helper.make_dir(output_folder)
 	num_chromHMM_state = helper.get_command_line_integer(sys.argv[4])
+	redo_existing_file = helper.get_command_line_integer(sys.argv[5])
+	assert redo_existing_file in [0,1], 'redo_existing_file should be 1 (yes, rewrite all the existing files in the output_folder, or 0 (no, only write files that have not been produced)'
 	print ("Done getting command line argument")
 	# call the function that will do the necessary work for this code
-	get_diff_rep_state_whole_genome(group1_folder, group2_folder, output_folder, num_chromHMM_state)
+	get_diff_rep_state_whole_genome(group1_folder, group2_folder, output_folder, num_chromHMM_state, redo_existing_file)
 	print ('Done!')
 
 def usage():
@@ -75,6 +97,7 @@ def usage():
 	print ("group2_folder: where the representative state maps for group2 are stored. The files in this fodler correspond to different regions in the genome")
 	print ("output_folder: where the difference between the representative state maps of two groups are stored. The files in this folder correspon to different regions in the genome")
 	print ('num_chromHMM_state')
+	print ('redo_existing_file: 0 or 1: 1 (yes, rewrite all the existing files in the output_folder, or 0 (no, only write files that have not been produced)')
 	print ("This code will calculate the difference of chromatin state assignment probabilities between two groups. The two groups should already had their representative chromatin state maps being calcualted. Output is a matrix, rows: genomic bins, columns: states")
 	exit(1)
 
